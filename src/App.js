9063be0cad6edf1a361ba8575cb4aa06f6379b78
CyanXshadow <bluexshadow6@gmail.com>
2023-10-15 00:16:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Task5 from "./components/Task5"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Task5 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
